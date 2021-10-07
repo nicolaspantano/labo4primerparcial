579d05935622c3f063b603bc44b176c0912b9b0f
nicolaspantano <nicolaspantano757@gmail.com>
2021-10-07 20:58:17 -0300

    2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ActorAltaComponent } from './componentes/actor-alta/actor-alta.component';
 import { ActorListadoComponent } from './componentes/actor-listado/actor-listado.component';
-import { ActorPeliculaComponent } from './componentes/actor-pelicula/actor-pelicula.component';
-import { BusquedaComponent } from './componentes/busqueda/busqueda.component';
 import { HomeComponent } from './componentes/home/home.component';
-import { PeliculaAltaComponent } from './componentes/pelicula-alta/pelicula-alta.component';
-import { PeliculaListadoComponent } from './componentes/pelicula-listado/pelicula-listado.component';
 import { LoginComponent } from './components/login/login.component';
 import { UserGuard } from './guards/user.guard';
 
@@ -14,26 +10,16 @@ const routes: Routes = [
   { path: '', redirectTo: 'bienvenido', pathMatch: 'full' },
   { path: 'login', component:LoginComponent},
   {
-    path: 'peliculas',
-    children:
-      [
-        { path: '', redirectTo: 'alta', pathMatch: 'full' },
-        { path: 'alta', component: PeliculaAltaComponent },
-        { path: 'listado', component: PeliculaListadoComponent }
-      ]
-  },
-  {
     path: 'producto',
     children:
       [
         { path: '', redirectTo: 'alta', pathMatch: 'full' },
         { path: 'alta', component: ActorAltaComponent, canActivate: [UserGuard] },
-        { path: 'listado', component: ActorListadoComponent },
-        { path: 'actorpelicula', component: ActorPeliculaComponent }
+        { path: 'listado', component: ActorListadoComponent }
+        
       ]
   },
   { path: 'bienvenido', component: HomeComponent },
-  { path: 'busqueda', component: BusquedaComponent },
 
   { path: '**', pathMatch: 'full', redirectTo: 'bienvenido' },
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,18 +15,15 @@ import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './componentes/home/home.component';
-import { BusquedaComponent } from './componentes/busqueda/busqueda.component';
-import { PeliculaAltaComponent } from './componentes/pelicula-alta/pelicula-alta.component';
+
 import { ActorAltaComponent } from './componentes/actor-alta/actor-alta.component';
 import { ActorListadoComponent } from './componentes/actor-listado/actor-listado.component';
-import { PeliculaListadoComponent } from './componentes/pelicula-listado/pelicula-listado.component';
-import { TablaPeliculaComponent } from './componentes/tabla-pelicula/tabla-pelicula.component';
+
 import { DetallePeliculaComponent } from './componentes/detalle-pelicula/detalle-pelicula.component';
 import { TablaPaisesComponent } from './componentes/tabla-paises/tabla-paises.component';
 import { TablaActorComponent } from './componentes/tabla-actor/tabla-actor.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-import { ActorPeliculaComponent } from './componentes/actor-pelicula/actor-pelicula.component';
 import { DetallePaisComponent } from './componentes/detalle-pais/detalle-pais.component';
 import { DetalleActorComponent } from './componentes/detalle-actor/detalle-actor.component';
 import { LoginComponent } from './components/login/login.component';
@@ -37,16 +34,11 @@ import { UnPaisDetalleComponent } from './components/un-pais-detalle/un-pais-det
   declarations: [
     AppComponent,
     HomeComponent,
-    BusquedaComponent,
-    PeliculaAltaComponent,
     ActorAltaComponent,
     ActorListadoComponent,
-    PeliculaListadoComponent,
-    TablaPeliculaComponent,
     DetallePeliculaComponent,
     TablaPaisesComponent,
     TablaActorComponent,
-    ActorPeliculaComponent,
     DetallePaisComponent,
     DetalleActorComponent,
     LoginComponent,
